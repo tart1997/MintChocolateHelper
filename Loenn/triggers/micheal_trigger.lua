114f3ab8fff4e48689145dc4e7f772afc624b1fc
tart1997 <feelingtart@gmail.com>
2026-03-23 02:23:05 -0500

    Micheal

---
+++ Loenn/triggers/micheal_trigger.lua
@@ -0,0 +1,13 @@
+local MichealTrigger = {}
+
+MichealTrigger.name = "MintChocolateHelper/MichealTrigger"
+
+MichealTrigger.placements = {
+    name = "micheal_trigger",
+    data = {
+        width = 16,
+        height = 16,
+    }
+}
+
+return MichealTrigger
---
+++ Source/Triggers/MichealTrigger.cs
@@ -0,0 +1,21 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.MintChocolateHelper.Triggers;
+
+[CustomEntity("MintChocolateHelper/MichealTrigger")]
+
+public class MichealTrigger : Trigger
+{
+    public MichealTrigger(EntityData data, Vector2 offset) : base(data, offset)
+    {
+        
+    }
+    
+    public override void OnEnter(Player player)
+    {
+        base.OnEnter(player);
+
+        Audio.Play("event:/MintChocolateHelper/Michael");
+    }
+}
