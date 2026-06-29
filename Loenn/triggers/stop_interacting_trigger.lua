fd6df4ff33d457505f61f44e8b413daa8c041a8c
tart1997 <robertbennettnumerouno@gmail.com>
2026-06-29 02:29:47 -0500

    Added Stop Interacting Trigger

---
+++ Loenn/triggers/stop_interacting_trigger.lua
@@ -0,0 +1,13 @@
+local StopInteractingTrigger = {}
+
+StopInteractingTrigger.name = "MintChocolateHelper/StopInteractingTrigger"
+
+StopInteractingTrigger.placements = {
+    name = "stop_interacting_trigger",
+    data = {
+        width = 16,
+        height = 16,
+    }
+}
+
+return StopInteractingTrigger
---
+++ Source/Triggers/StopInteractingTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.MintChocolateHelper.Triggers;
+[CustomEntity("MintChocolateHelper/StopInteractingTrigger")]
+
+public class StopInteractingTrigger : Trigger
+{
+    public StopInteractingTrigger(EntityData data, Vector2 offset) : base(data, offset)
+    {
+    }
+    
+    public override void OnEnter(Player player)
+    {
+        base.OnEnter(player);
+        if (Scene is not Level level) return;
+
+        foreach (Lookout lookout in level.Tracker.GetEntitiesTrackIfNeeded<Lookout>().Cast<Lookout>())
+        {
+            lookout.StopInteracting();
+        }
+    }
+}
