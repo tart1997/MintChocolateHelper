f41f306ee33c7661a0be14b51858c2e158073745
tart1997 <robertbennettnumerouno@gmail.com>
2026-07-25 17:08:59 -0500

    untest

--- Source/MintChocolateHelperModuleSettings.cs
+++ Source/MintChocolateHelperModuleSettings.cs
@@ -2,6 +2,5 @@ namespace Celeste.Mod.MintChocolateHelper;
 
 public class MintChocolateHelperModuleSettings : EverestModuleSettings
 {
-    public bool test;
     public bool ShowCustomDashIndicators {get; set;} = true;
 }
